afa90075791dd23b04b47a6fabd6615f332fd91b
Simon Vlcek <vlcek@cyber-guard.com>
2025-06-16 08:56:46 +0200

    Fixing bug

---
+++ _data/_data/_data/sites/undrr-esri-sites.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: "UNDRR Esri sites", // optional, falls back to object key
+	description: "UNDRR Esri sites",
+	options: {
+		runs: 2,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		"https://rix.undrr.org/",
+		// "",
+	]
+};
---
+++ _data/_data/_data/sites/undrr-java-sites.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "UNDRR Java sites", // optional, falls back to object key
+	description: "UNDRR Java sites",
+	options: {
+		runs: 2,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		"https://mcr2030dashboard.undrr.org/",
+		"https://sendaicommitments.undrr.org/",
+		"https://sendaimonitor.undrr.org/",
+		// "",
+	]
+};
