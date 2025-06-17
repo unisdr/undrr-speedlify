48eb87d2c77f6079f94bb04d3a23bb89ee3f7c13
Ken Hawkins <kenneth.hawkins@un.org>
2025-06-17 10:21:13 +0200

    bug: fix build, try 2

--- _data/sites/undrr-java-sites.js
+++ _data/sites/undrr-java-sites.js
@@ -7,8 +7,8 @@ module.exports = {
 	},
 	urls: [
 		"https://mcr2030dashboard.undrr.org/",
-		"https://sendaicommitments.undrr.org/",
-		"https://sendaimonitor.undrr.org/",
+		// "https://sendaicommitments.undrr.org/",
+		// "https://sendaimonitor.undrr.org/",
 		// "",
 	]
 };
